f78de37b8d4adb0143bf955495eb45ae919951e2
Harry Fischer <harry.fischer@guardian.co.uk>
2020-01-09 14:34:41 +0000

    adding test code for two thrashers that change on 4s interval

--- .build/default/app.js
+++ .build/default/app.js
@@ -93,13 +93,19 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// data capture for ophan
-function trackLoad() {
-  window.guardian.ophan.record({
-    component: 'thrasher : add-your-thrasher-name : load',
-    value: 1
-  });
-}
+console.log("running"); // setInterval(() => {
+//     document.querySelector('.thrasher-1').classList.toggle('class-toggle')
+// }, 4000)
+// setInterval(() => {
+//     document.querySelector('.thrasher-2').classList.toggle('class-toggle')
+// }, 4000)
+
+var thrasher1 = document.querySelector('.thrasher-1');
+var thrasher2 = document.querySelector('.thrasher-2');
+var timeInterval = setInterval(function () {
+  thrasher1.classList.toggle('class-toggle');
+  thrasher2.classList.toggle('class-toggle');
+}, 4000);
 
 /***/ }),
 
--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,20 @@
-// data capture for ophan
-function trackLoad() {
-    window.guardian.ophan.record({
-        component: 'thrasher : add-your-thrasher-name : load',
-        value: 1
-    });
-}
+console.log("running")
 
+// setInterval(() => {
+//     document.querySelector('.thrasher-1').classList.toggle('class-toggle')
+// }, 4000)
+
+// setInterval(() => {
+//     document.querySelector('.thrasher-2').classList.toggle('class-toggle')
+// }, 4000)
+
+
+
+
+const thrasher1 = document.querySelector('.thrasher-1')
+const thrasher2 = document.querySelector('.thrasher-2')
+
+const timeInterval = setInterval(() => {
+    thrasher1.classList.toggle('class-toggle')
+    thrasher2.classList.toggle('class-toggle')
+}, 4000)
