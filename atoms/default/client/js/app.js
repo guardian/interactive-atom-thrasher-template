d27d7a82de9ed1b0703bae7b55d510473e4fa310
Harry Fischer <harry.fischer@guardian.co.uk>
2020-07-13 17:36:09 +0100

    adding black history timeline thrasher

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,32 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : add-your-thrasher-name : load',
+        component: 'thrasher : black-history-timeline : load',
         value: 1
     });
 }
 
+
+
+
+
+var thrasher = document.querySelector('.black-history-timeline');
+
+var isInViewport = function (thrasher) {
+    var bounding = thrasher.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+
+document.addEventListener('scroll', function (event) {
+    if (isInViewport(thrasher)) {
+        thrasher.classList.add('step1');
+    } else {
+        thrasher.classList.remove('step1');
+    }
+});
