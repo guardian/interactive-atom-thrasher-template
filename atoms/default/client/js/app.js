a3df60cc3eab99fbe6e59d6b25f40960a540df80
Harry Fischer <harry.fischer@guardian.co.uk>
2020-07-16 10:03:59 +0100

    first commit

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,83 @@
 // data capture for ophan
 function trackLoad() {
-    window.guardian.ophan.record({
-        component: 'thrasher : add-your-thrasher-name : load',
-        value: 1
-    });
+  window.guardian.ophan.record({
+    component: "thrasher : add-your-thrasher-name : load",
+    value: 1,
+  });
 }
 
+const allCovers = document.querySelector(".best-music__covers");
+const albumCovers = document.querySelectorAll(".best-music__covers__image");
+let currentCover = 0;
+let z = 1;
+
+// move covers on load
+allCovers.addEventListener("load", loadMove());
+
+function loadMove() {
+  albumCovers.forEach((cover) => {
+    const x = 600 * Math.random() - 25;
+    const r = 30 * Math.random() - 20;
+    const y = 20 * Math.random() - 40;
+    cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
+  });
+}
+
+// move covers to top on click
+document.addEventListener("scroll", function (event) {
+  var currentCover = currentCover + 1;
+  if (currentCover > albumCovers.length - 1) {
+    currentCover = 0;
+  }
+  z = z + 1;
+  albumCovers[currentCover].style.zIndex = z;
+});
+
+// var thrasher = document.querySelector(".best-music");
+
+// var isInViewport = function (thrasher) {
+//   var bounding = thrasher.getBoundingClientRect();
+//   return (
+//     bounding.top >= 0 &&
+//     bounding.left >= 0 &&
+//     bounding.bottom <=
+//       (window.innerHeight || document.documentElement.clientHeight) &&
+//     bounding.right <=
+//       (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// };
+
+// const allCovers = document.querySelector(".best-music__covers");
+// const albumCovers = document.querySelectorAll(".best-music__covers__image");
+// let currentCover = 0;
+// let z = 1;
+
+// // move covers on load
+// // allCovers.addEventListener("load", loadMove());
+
+// function loadMove() {
+//   albumCovers.forEach((cover) => {
+//     const x = 600 * Math.random() - 25;
+//     const r = 30 * Math.random() - 5;
+//     const y = 20 * Math.random() - 40;
+//     cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
+//   });
+// }
+
+// function moveBack() {
+//   albumCovers.forEach((cover) => {
+//     const x = 0;
+//     const r = 0;
+//     const y = 0;
+//     cover.style.transform = `translate(${x}px, ${y}px) rotate(${r}deg)`;
+//   });
+// }
+
+// document.addEventListener("scroll", function (event) {
+//   if (isInViewport(thrasher)) {
+//     thrasher.classList.add("step1");
+//     loadMove();
+//   } else {
+//     moveBack();
+//   }
+// });
--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,5 +1,21 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+// import mainHTML from "./atoms/default/server/templates/main.html!text"
 
-export async function render() {
-    return mainHTML;
-} 
+// export async function render() {
+//     return mainHTML;
+// } 
+
+import mainTemplate from "./atoms/default/server/templates/main.html!text"
+import Mustache from 'mustache'
+import rp from 'request-promise'
+
+export function render() {
+    return rp({
+        uri: 'https://interactive.guim.co.uk/docsdata-test/19EgJWnDc_zEKgPj3C-jjNio58yVPuDdJpMIar0qEe3o.json',
+        json: true
+    }).then((data) => {
+        var sheets = data.sheets;
+        console.log(sheets);
+        var html = Mustache.render(mainTemplate, sheets);
+        return html;
+    });
+}
