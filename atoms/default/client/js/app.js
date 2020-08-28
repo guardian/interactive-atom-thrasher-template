1c79828fb0477630c5601ceece5b728e18add90a
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2020-08-28 15:04:51 +0200

    base code

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,39 @@
+function checkExists(startThrasherFunction) {
+    var checkInterval = setInterval(function () {
+        if (document.querySelector('.uni-guide-2021')) {
+            startThrasherFunction();
+            clearInterval(checkInterval);
+        }
+    }, 100);
+}
+
+checkExists(thrasherMain);
+
+
+function thrasherMain() {
+    trackLoad();
+    selectVersion();
+}
+
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : add-your-thrasher-name : load',
+        component: 'thrasher : uni guide 2021 : load',
         value: 1
     });
 }
+
+function selectVersion() {
+    // 50-50 split between
+    // n=1 and n=2
+    let n = Math.floor(Math.random() * 2) + 1;
+
+    const thrasherEl = document.querySelector('.uni-guide-2021');
+    console.log('hi', thrasherEl);
+    if (thrasherEl) {
+        console.log('it exists');
+        thrasherEl.classList.add(`version-${n}`);
+    }
+}
+
 
