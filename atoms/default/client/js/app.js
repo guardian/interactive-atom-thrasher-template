4ca0670f27be3063ec0cadd4c830b7b4eff46fc4
Alexandre Dufournet <alex.dufournet@guardian.co.uk>
2020-11-09 15:08:48 +0000

    Implement the new designs

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : first-thing-election-special : load',
+        component: 'thrasher : us-morning-briefing-post-election-2020 : load',
         value: 1
     });
 }
