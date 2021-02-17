f9185ba789d193b46fa3e3e54e46e51e468ebf24
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-02-17 16:44:19 +0000

    Untold Chaos documentary thraher

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -6,3 +6,19 @@ function trackLoad() {
     });
 }
 
+const docthrasher = document.querySelector("#doc-untold-chaos");
+const animClass = "dw-zoomin";
+
+var observer = new IntersectionObserver(function(entries) {
+	if(entries[0].isIntersecting === true) {
+    docthrasher.classList.add(animClass);
+		// console.log('Element is fully visible in screen');
+  } else if(entries[0].isIntersecting === false){
+    docthrasher.classList.remove(animClass);
+    // console.log('Gone');
+  }
+}, { threshold: [1] });
+
+// document.addEventListener("DOMContentLoaded", function(event) {
+  observer.observe(docthrasher);
+// });
