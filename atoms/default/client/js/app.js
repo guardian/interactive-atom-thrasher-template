e24c41a9fbe480cafbc4c72fd13d9b445866599c
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2021-03-02 10:54:01 +0100

    adds subscriber-hiding cookie code

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,23 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : add-your-thrasher-name : load',
+        component: 'thrasher : digital-subscription-ideas : load',
         value: 1
     });
 }
 
+function getCookieValue(name) {
+    var val = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+    return val ? val.pop() : undefined;
+}
+
+function shouldHideSupportMessaging() {
+    return getCookieValue('gu_hide_support_messaging') === 'true';
+}
+
+
+if (shouldHideSupportMessaging()) {
+    document.getElementById("digital-subscription").style.display = "none";
+} else {
+    document.getElementById("digital-subscription").style.display = "block";
+}
