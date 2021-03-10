301714ccb4339163b6a2e9b991325f11e8aae618
Harry Fischer <harry.fischer@guardian.co.uk>
2021-03-10 20:01:21 +0000

    base styles done

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : hf-culture-best-of-month : load',
         value: 1
     });
 }
