dda205d8f2018c8aeb620f685d7be7bae44145e0
Harry Fischer <harry.fischer@guardian.co.uk>
2021-03-10 15:23:50 +0000

    first version

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,23 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : harry-test-thrasher : load',
         value: 1
     });
 }
 
+function getCookieValue(name) {
+    var val = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+    return val ? val.pop() : undefined;
+}
+
+function shouldHideSupportMessaging() {
+    return getCookieValue('gu_hide_support_messaging') === 'true';
+}
+
+
+if (shouldHideSupportMessaging()) {
+    document.getElementById("digital-subscription").style.display = "none";
+} else {
+    document.getElementById("digital-subscription").style.display = "block";
+}
