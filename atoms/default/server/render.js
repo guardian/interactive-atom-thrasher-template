ff921bd82fcb133f3606b20d0b80d2c7aae806db
Harry Fischer <harry.fischer@guardian.co.uk>
2021-03-29 16:26:12 +0100

    add images

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -3,3 +3,20 @@ import mainHTML from "./atoms/default/server/templates/main.html!text"
 export async function render() {
     return mainHTML;
 } 
+
+
+// import mainTemplate from "./atoms/default/server/templates/main.html!text"
+// import Mustache from 'mustache'
+// import rp from 'request-promise'
+
+// export function render() {
+//     return rp({
+//         uri: 'https://interactive.guim.co.uk/docsdata-test/1QkO66GhLFBKPtaNGhzNtDQfoxCxpbBAi4tK8IpkVE6Y.json',
+//         json: true
+//     }).then((data) => {
+//         var sheets = data.sheets;
+//         console.log(sheets);
+//         var html = Mustache.render(mainTemplate, sheets);
+//         return html;
+//     });
+// }
