497b768e6259d6a41815cafad866584aee7da690
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-03-29 16:47:01 +0100

    Thrasher test

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : dw-weekly-mag-sub-mar-21 : load',
         value: 1
     });
 }
