121cc2cde61753c2858166f424de475ff5730258
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-05-11 14:43:08 +0100

    A Space to be rebuilt

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,22 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : add-your-thrasher-name : load',
+        component: 'thrasher : aspacetobe : load',
         value: 1
     });
 }
 
+const thrasherImgContainer = document.querySelector(".aspacetobe__image");
+const dwImgContainerClass = "dw-zoom-container";
+
+var observer = new IntersectionObserver(function(entries) {
+	if(entries[0].isIntersecting === true) {
+    thrasherImgContainer.classList.add(dwImgContainerClass);
+		// console.log('Element is fully visible in screen');
+  } else if(entries[0].isIntersecting === false){
+    thrasherImgContainer.classList.remove(dwImgContainerClass);
+    // console.log('Gone');
+  }
+}, { threshold: [1] });
+
+observer.observe(thrasherImgContainer);
