4878e8a9d47a6fbcf880764f9d77db9c81af2db1
Harry Fischer <harry.fischer@guardian.co.uk>
2021-05-24 13:52:46 +0100

    basic set up

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : rights-and-freedom-header : load',
         value: 1
     });
 }
