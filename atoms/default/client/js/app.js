630811a79702b8d032c16118fad53c9eef81d1f3
pradasa <ana.pradas@gmail.com>
2021-05-24 18:05:59 +0100

    added main image and some js

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,18 @@
 // data capture for ophan
-function trackLoad() {
-    window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
-        value: 1
-    });
-}
+// function trackLoad() {
+//     window.guardian.ophan.record({
+//         component: 'thrasher : thrasher-name : load',
+//         value: 1
+//     });
+// }
 
+console.log('hello world');
+
+var myfirstvariable = 'im writing js';
+
+console.log(myfirstvariable);
+
+var element = document.querySelector('.inner-wrapper');
+console.log(element);
+
+element.classList.add('active');
--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,4 +1,4 @@
-import mainHTML from "./atoms/default/server/templates/main.html!text"
+import mainHTML from "./atoms/default/server/templates/blank.html!text"
 
 export async function render() {
     return mainHTML;
