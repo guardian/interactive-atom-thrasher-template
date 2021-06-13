485381c50853f402539e66ca6dd02035ff100aa4
pradasa <ana.pradas@gmail.com>
2021-06-13 13:53:01 +0100

    BEMified Grace's thrasher and made it mobile 1st. Missing to-dos are sorting image z-index and desktop

--- atoms/default/server/render.js
+++ atoms/default/server/render.js
@@ -1,4 +1,4 @@
-import mainHTML from "./atoms/default/server/templates/blank.html!text"
+import mainHTML from "./atoms/default/server/templates/main.html!text"
 
 export async function render() {
     return mainHTML;
