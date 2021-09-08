90214a62bb5f06f89015e3eb9057e67e7f5fa943
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-09-08 18:47:43 +0100

    uni guide thrasher with css animation

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : dw-uni-guide-2021-v2 : load',
         value: 1
     });
 }
