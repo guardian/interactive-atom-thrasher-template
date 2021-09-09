e2fea789e013c1c7501e1cfbcec545a6d608c345
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-09-09 18:10:20 +0100

    Uniguide 2 based on landing

---
+++ atoms/uniguide/client/js/app.js
@@ -0,0 +1,8 @@
+// data capture for ophan
+function trackLoad() {
+    window.guardian.ophan.record({
+        component: 'thrasher : dw-uni-guide-2021-v2 : load',
+        value: 1
+    });
+}
+
---
+++ atoms/uniguide/server/render.js
@@ -0,0 +1,5 @@
+import mainHTML from "./atoms/uniguide/server/templates/main.html!text"
+
+export async function render() {
+    return mainHTML;
+}
