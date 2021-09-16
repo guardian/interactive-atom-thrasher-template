c8170a85a7814faf5dd8b7d88bd4d811ec07dcc1
David Lawes <david.lawes@guardian.co.uk>
2021-09-16 17:49:06 +0100

    add climate hope section
    we now have 3 sections defined in front-climate-crisis. I've picked
    some articles I thought looked interesting from the google doc, the
    titles, standfirsts and links have copied across but I got stuck
    thinking about the image sources. Also in app.js I've added a small
    function as a test to think about how to change the styling of the
    three sections based on a click event. Hopefully it makes sense!

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,8 +1,23 @@
-// data capture for ophan
-function trackLoad() {
-    window.guardian.ophan.record({
-        component: 'thrasher : hack2021 : load',
-        value: 1
-    });
-}
+const climateBlockIds = [
+    'climate-crisis',
+    'climate-fight',
+    'climate-hope',
+];
 
+const buttons = document.getElementsByClassName('thrasher-button');
+
+for (let button of buttons) {
+    button.addEventListener('click', function() {
+        const id = button.getAttribute('id').replace('__button', '');
+        document.getElementById(id).style.display = 'block';
+        // + apply styling
+
+        climateBlockIds.forEach(blockId => {
+            if (blockId !== id) {
+                document.getElementById(blockId).style.display = 'none';
+                // change text to grey
+                // change bg colour
+            }
+        })
+    })
+}
