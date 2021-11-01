0a364bfc7d4c67a07df4f845ddda6e9760e09d8e
Dom Whooley <dom.whooley.casual@guardian.co.uk>
2021-11-01 11:01:32 +0000

    Subscriber signed in cookies

--- atoms/thrasher/client/js/app.js
+++ atoms/thrasher/client/js/app.js
@@ -77,3 +77,19 @@ function browserResize() {
 
 window.onresize = browserResize;
 preloadImages();
+
+
+function getCookieValue(name) {
+    var val = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
+    return val ? val.pop() : undefined;
+}
+
+function shouldHideSupportMessaging() {
+    return getCookieValue('gu_hide_support_messaging') === 'true';
+}
+
+if (shouldHideSupportMessaging()) {
+    document.getElementById("digital-subscription").style.display = "none";
+} else {
+    document.getElementById("digital-subscription").style.display = "block";
+}
