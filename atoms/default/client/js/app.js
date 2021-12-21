b38ef6704dec4ea5ea3360dde2cae58049193f14
George B <705427+georgeblahblah@users.noreply.github.com>
2021-12-21 14:12:04 +0000

    New thrasher setup

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : pushing-buttons-signup : load',
         value: 1
     });
 }
