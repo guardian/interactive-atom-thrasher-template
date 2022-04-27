8a93272a2078b616f515b018bb018205ece5d378
David Blatcher <dv.bltchr@gmail.com>
2022-04-27 10:25:09 +0100

    create new thrasher, copy and adapt code from first edition

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : morning-mail-thrasher : load',
         value: 1
     });
 }
