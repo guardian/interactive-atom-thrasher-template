f38989681a8e7bc2a05651c03a6c41866c721075
David Blatcher <dv.bltchr@gmail.com>
2022-04-28 09:55:44 +0100

    copy from morning mail and adapt

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : first-thing-thrasher : load',
         value: 1
     });
 }
