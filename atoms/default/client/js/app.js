c23119ce6b82e7c7f0e485f4a74467621366f74e
Catriona Morrison <catriona.morrison@guardian.co.uk>
2022-09-06 18:03:02 +0100

    gulpfile

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,12 +1,12 @@
-// data capture for ophan
-function trackLoad() {
-    window.guardian.ophan.record({
-        component: 'thrasher : 2022-documentary-template : load',
-        value: 1
-    });
-}
+// // data capture for ophan
+// function trackLoad() {
+//   window.guardian.ophan.record({
+//     component: 'thrasher : 2022-documentary-template : load',
+//     value: 1,
+//   });
+// }
 
-// THIS FUNCTION CHECKS IF A USER IS A SUBSCRIBER AND HIDES THE THRASHER. 
+// THIS FUNCTION CHECKS IF A USER IS A SUBSCRIBER AND HIDES THE THRASHER.
 // UPDATE THE CONTAINER NAME ON LINES 21 & 23
 // function getCookieValue(name) {
 //     var val = document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)');
@@ -25,12 +25,12 @@ function trackLoad() {
 // const pauseSvg = document.querySelector('.documentary-template-2022__content-wrapper__pause-button svg.pause');
 // const playSvg = document.querySelector('.documentary-template-2022__content-wrapper__pause-button svg.play');
 const docthrasher = document.querySelector('[id^="thrasher__documentary-template-2022"]');
-var vid = document.getElementById("docVideo");
-const animClass = "dw-video";
+const vid = document.getElementById('docVideo');
+const animClass = 'dw-video';
 // let playing = false;
 
 // Grab the prefers reduced media query.
-const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
+const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
 
 function playVid() {
   vid.play();
@@ -46,42 +46,36 @@ function pauseVid() {
   // playing = false;
 }
 
-var observer = {
-  root: document.body,
-  rootMargin: "0px"
-};
-
-var observer = new IntersectionObserver(function(entries) {
-	if(entries[0].isIntersecting === true) {
+const observer = new IntersectionObserver(((entries) => {
+  if (entries[0].isIntersecting === true) {
     docthrasher.classList.add(animClass);
     playVid();
-  } else if(entries[0].isIntersecting === false){
+  } else if (entries[0].isIntersecting === false) {
     docthrasher.classList.remove(animClass);
     pauseVid();
   }
-}, { threshold: [0.95] });
+}), {
+  root: document.body,
+  rootMargin: '0px',
+  threshold: [0.95],
+});
 
 // playSvg.classList.add('active');
 // pauseSvg.classList.remove('active');
 
-
 if (mediaQuery.matches) {
   // Turn video off
   pauseVid();
+} else if (document.body.classList.contains('ios') || document.body.classList.contains('android')) {
+  playVid();
+  vid.setAttribute('autoplay', '');
 } else {
-  // turn video on
-  if ( document.body.classList.contains("ios") || document.body.classList.contains("android" )) {
-    playVid();
-    vid.setAttribute("autoplay", "");
-  } else {
-    observer.observe(docthrasher);
-  }
+  observer.observe(docthrasher);
 }
 // pauseButton.addEventListener('click', () => {
-//   if (playing) { 
+//   if (playing) {
 //     pauseVid();
-//   } else { 
+//   } else {
 //     playVid();
 //   }
 // });
-
