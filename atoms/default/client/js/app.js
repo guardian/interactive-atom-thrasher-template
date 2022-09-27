898f9707006bde4461843ecf92221ba9e9530397
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2022-09-27 09:18:33 +0100

    Initial version of thrasher (copying First Edition)

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : house-to-home : load',
         value: 1
     });
 }
