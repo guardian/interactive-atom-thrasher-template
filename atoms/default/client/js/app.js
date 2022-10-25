00c72cd5c080d2bbf785f356426ba290339be5b3
David Blatcher <dv.bltchr@gmail.com>
2022-10-25 12:59:18 +0100

    add the invert colors scripts

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -110,4 +110,35 @@ function initCardSections() {
   });
 }
 
+function invertColorOnIframeResultPages(event) {
+  if (!event.data.subject || event.data.subject !== "emailEmbedPageLoaded") {
+    return;
+  }
+  if (!event.data.path || typeof event.data.path !== "string") {
+    return;
+  }
+  if (
+    !event.data.path.startsWith("/email/error") &&
+    !event.data.path.startsWith("/email/success")
+  ) {
+    return;
+  }
+  if (event.origin !== window.origin) {
+    console.warn('emailEmbedPageLoaded received from wrong origin:', event.origin)
+    return;
+  }
+  var iframesOnDarkBackgrounds = [
+    ...document.querySelectorAll(
+      ".newsletter-card__iframe-wrapper iframe.invert-colors"
+    ),
+  ];
+
+  iframesOnDarkBackgrounds.forEach(function (iframe) {
+    if (iframe.contentWindow === event.source) {
+      iframe.style.filter = "invert(1)";
+    }
+  });
+}
+
+window.addEventListener("message", invertColorOnIframeResultPages, false);
 initCardSections();
