29b814bb91888bb0dddf451d91ab738653e67f5a
Rachel Miles <74301289+codingWithRach@users.noreply.github.com>
2022-12-22 08:31:28 +0000

    Create basic thrasher

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -1,7 +1,7 @@
 // data capture for ophan
 function trackLoad() {
     window.guardian.ophan.record({
-        component: 'thrasher : thrasher-name : load',
+        component: 'thrasher : fashion-statement-thrasher : load',
         value: 1
     });
 }
